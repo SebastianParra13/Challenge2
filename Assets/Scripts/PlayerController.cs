d7c4d3a75088d5dc8c62ef8bcb7f2a944ba29679
SebastianParra13 <35278366+SebastianParra13@users.noreply.github.com>
2019-03-10 22:27:24 -0400

    Unity Files for Challenge 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    private Transform PlayerTransform;
+
+    public float speed;
+    public float jumpForce;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text failText;
+    public Transform TeleportGoal;
+    public Camera Cam1;
+    public Camera Cam2;
+    public GameObject Enemy;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        failText.text = "";
+        SetCountText();
+        SetLivesText();
+        PlayerTransform = GameObject.Find("Player").transform;
+        Cam1.enabled = true;
+        Cam2.enabled = false;
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+        if (lives <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (count == 4)
+        {
+            PlayerTransform.position = TeleportGoal.position;
+            Cam1.enabled = false;
+            Cam2.enabled = true;
+            lives = 3;
+            SetLivesText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+            Enemy.SetActive(false);
+            musicSource.clip = musicClipOne;
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            musicSource.loop = false;
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            failText.text = "GAME OVER";
+        }
+    }
+}
